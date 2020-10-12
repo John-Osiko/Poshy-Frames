cd111c0b2b2b7127a45e3d4d46a04b31db1ebcaf
John-Osiko <johnmaxosiko@gmail.com>
2020-10-12 05:11:21 +0300

    Added url for viewing by location

--- frames/urls.py
+++ frames/urls.py
@@ -7,4 +7,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/', views.search_results, name='search'),
-]    
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
